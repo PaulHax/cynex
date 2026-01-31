3bd0f85895c52e35cdc4f82b3a35f7dd97313c8a
Paul Elliott <paul@vizworkshop.com>
2026-01-31 13:04:12 -0500

    Auto-bump patch version on merge to main

--- mlflow-integration/src/mlflow_cynex/__init__.py
+++ mlflow-integration/src/mlflow_cynex/__init__.py
@@ -1,10 +1,12 @@
 """MLflow integration for Cynex trajectory viewer."""
 
+from importlib.metadata import version
+
 from mlflow_cynex.logging import (
     TrajectoryValidationError,
     log_trajectory,
     validate_trajectory,
 )
 
-__version__ = "0.1.0"
+__version__ = version("mlflow-cynex")
 __all__ = ["log_trajectory", "validate_trajectory", "TrajectoryValidationError"]
